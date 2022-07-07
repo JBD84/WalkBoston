55a00ddfa148adb11ba35a90e1e5ece678096e4c
Javier Bambaren D <74006646+JBD84@users.noreply.github.com>
2022-07-07 13:31:20 -0500

    Add files via upload

---
+++ Walk Boston/walkboston.js
@@ -0,0 +1,51 @@
+function renderTopSalaries(boston, container) {
+  const people = boston.data;     
+   var topSalaries = people.sort((a,b) => b[11] -a[11]).slice(0,5);
+
+   const len = topSalaries.length;
+   let html = '';
+  for (let i = 0; i < len; i++) {
+    html +=
+      '<li class="post">' + '<h2>' + topSalaries[i][8] + '</h2>' + '<h3>' + topSalaries[i][11] + '</h3>';
+  }
+
+  // TODO: add code to display the html variable inside a ul element with id="data"
+  container.innerHTML = '<ul id="topSalaries">' + html + '</ul>';
+
+  // Hint: you can use the container parameter's innerHTML property to insert Html tags
+}
+renderTopSalaries(boston, document.getElementById('container'));
+
+function renderPosts(boston, container) {
+  const people = boston.data;
+  const len = boston.data.length;
+  let html = '';
+  for (let i = 0; i < len; i++) {
+    html +=
+      '<li class="post">' + '<h2>' + people[i][8] + '</h2>' + '<h3>' + people[i][11] + '</h3>';
+  }
+
+  // TODO: add code to display the html variable inside a ul element with id="data"
+  container.innerHTML = '<ul id="data">' + html + '</ul>';
+
+  // Hint: you can use the container parameter's innerHTML property to insert Html tags
+}
+renderPosts(boston, document.getElementById('container'));
+
+
+function renderTopEmployees(boston, container) {
+  const people = boston.data;     
+   var topEmployees = people.filter((a) => a[11] > 200000);
+   const len = topEmployees.length;
+   let html = '';
+  for (let i = 0; i < len; i++) {
+    html +=
+      '<li class="post">' + '<h2>' + topEmployees[i][8] + '</h2>' + '<h3>' + topEmployees[i][11] + '</h3>';
+  }
+
+  // TODO: add code to display the html variable inside a ul element with id="data"
+  container.innerHTML = '<ul id="topEmployees">' + html + '</ul>';
+
+  // Hint: you can use the container parameter's innerHTML property to insert Html tags
+}
+renderTopEmployees(boston, document.getElementById('container'));
